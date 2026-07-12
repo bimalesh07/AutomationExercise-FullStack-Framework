2c5a223ba52f94e20fadd24b8dae18edd677d208
Bimalesh <bimaleshk07@gmail.com>
2026-07-12 23:59:53 +0530

    updated

--- Test_Case/test_ui/test_login_ddt.py
+++ Test_Case/test_ui/test_login_ddt.py
@@ -18,11 +18,23 @@ class TestLoginDDT:
         lp.navigate_loginpage()
         lp.Login_with_credentials(data['email'], data['password'])
 
-        if data['expected_result'] == "success":
+       #if  Data Valid hai 
+        if data['is_valid']:
             expect(lp.logout_visible).to_be_visible()
             logger.info("Valid Login Scenario Passed!")
-        else:
-            expect(lp.login_error_message()).to_have_text(data['expected_message'])
-            logger.info(f"Negative Scenario '{data['scenario']}' Passed Successfully!")
 
-        logger.info("******************DDT TEST ARE COMPLETED**************************")
+        #if Data Invalid hai
+        else:
+            # text validation hai
+            if data['error_type'] == "page":
+                expect(lp.login_error_message()).to_have_text(data['expected_message'])
+                logger.info(f"Red Page Error Verified for: {data['scenario']}")
+            
+            #  tooltip validation hai
+            elif data['error_type'] == "browser":
+                target_element = data['error_element']
+                actual_tooltip = page.locator(target_element).evaluate("el => el.validationMessage")
+                
+                #assertion
+                assert actual_tooltip == data['expected_message'], f"Expected '{data['expected_message']}' but got '{actual_tooltip}'"
+                logger.info(f"Browser Tooltip Verified for: {data['scenario']}")
