42c12a7f57a968ffbb63947823247f4ce92b849a
Bimalesh <bimaleshk07@gmail.com>
2026-07-13 01:18:52 +0530

    DDT Test Completed

--- Test_Case/test_ui/test_login_ddt.py
+++ Test_Case/test_ui/test_login_ddt.py
@@ -18,23 +18,20 @@ class TestLoginDDT:
         lp.navigate_loginpage()
         lp.Login_with_credentials(data['email'], data['password'])
 
-       #if  Data Valid hai 
         if data['is_valid']:
             expect(lp.logout_visible).to_be_visible()
             logger.info("Valid Login Scenario Passed!")
 
-        #if Data Invalid hai
         else:
-            # text validation hai
             if data['error_type'] == "page":
                 expect(lp.login_error_message()).to_have_text(data['expected_message'])
                 logger.info(f"Red Page Error Verified for: {data['scenario']}")
             
-            #  tooltip validation hai
+
             elif data['error_type'] == "browser":
                 target_element = data['error_element']
+                
                 actual_tooltip = page.locator(target_element).evaluate("el => el.validationMessage")
                 
-                #assertion
                 assert actual_tooltip == data['expected_message'], f"Expected '{data['expected_message']}' but got '{actual_tooltip}'"
                 logger.info(f"Browser Tooltip Verified for: {data['scenario']}")
